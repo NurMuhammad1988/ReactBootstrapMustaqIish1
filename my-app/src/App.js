731fd2535f9ca090ae81b5f48335b7034701c5c2
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-05-16 02:05:58 +0500

    problems with path and navbar styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,11 @@ import AppAbout from './components/About';
 import AppServices from './components/Services';
 import AppWorks from './components/Works';
 import AppTeams from './components/Teams';
+import AppTestimonials from './components/Testimonials';
+import AppPricing from './components/Pricing';
+import AppBlog from './components/Blog';
+import AppContact from './components/Contact';
+import AppFooter from './components/Footer';
 
 
 function App() {
@@ -24,7 +29,15 @@ function App() {
         <AppServices />
         <AppWorks />
         <AppTeams />
+        <AppTestimonials />
+        <AppPricing />
+        <AppBlog />
+        <AppContact />
       </main>
+
+      <footer id="footer">
+        <AppFooter />
+      </footer>
 
 
 
